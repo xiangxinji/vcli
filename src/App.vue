583f4392c53553f6cf95538562eef52750ca8014
e <xxjbbking@163.com>
2020-04-18 14:13:19 +0800

    增加了 vuex

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div class="view">
     <router-link to="/">Home</router-link>
     <router-link to="/target">Target</router-link>
+
+    {{ $store.state.Cart }}
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import router from './router/index.js'
+import store from './store/index.js'
 
 new Vue({
-  router , 
+  router ,
+  store ,  
   render: (h) => h(App),
 }).$mount("#vue-app");
